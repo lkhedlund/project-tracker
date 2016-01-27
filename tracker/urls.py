32ba8c5e33e550eb87e3dafc9369672b7192c3e1
Lars Hedlund <lkhedlund@gmail.com>
2016-01-27 11:19:06 -0800

    Basic detail page working. POST request needs fix

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
 from . import views
-from tracker.views import ProjectView
+from tracker.views import ProjectList, ProjectDetail
 
 urlpatterns = [
-    url(r'^$', ProjectView.as_view(), name="project_list"),
-    url(r'^project/(?P<pk>[0-9]+)/$', views.project_detail, name="project_detail"),
+    url(r'^$', ProjectList.as_view(), name="project_list"),
+    url(r'^project/(?P<pk>[0-9]+)/$', ProjectDetail.as_view(), name="project_detail"),
     url(r'^project/new/$', views.project_new, name="project_new"),
     url(r'^project/(?P<pk>[0-9]+)/edit/$', views.project_edit, name="project_edit"),
     url(r'^project/(?P<pk>[0-9]+)/delete/$', views.project_delete, name="project_delete"),
--- tracker/views.py
+++ tracker/views.py
@@ -10,56 +10,70 @@ from .models import Project, Count
 from .forms import ProjectForm, CountForm
 
 @method_decorator(login_required, name='dispatch')
-class ProjectView(View):
+class ProjectList(View):
+    projects = Project.objects.all()
+    template_name = 'tracker/project_list.html'
 
     def get(self, request):
-        projects = Project.objects.all()
-        return render(request, 'tracker/project_list.html', {
-            'projects': projects
+        return render(request, self.template_name, {
+            'projects': self.projects
             })
 
-@login_required
-def project_detail(request, pk):
-    # FIXME: Need to refactor into Class-based views
-    projects = Project.objects.all()
+@method_decorator(login_required, name='dispatch')
+class ProjectDetail(View):
+    form_class = CountForm
+    template_name = 'tracker/project_detail.html'
     project = get_object_or_404(Project, pk=pk)
-    counts = project.counts.all()
-    count_sum = project.counts.aggregate(Sum('count_update'))
-    # Variables for date_progress and words_per_day
-    sum_value = next(iter(count_sum.values()))
-    if sum_value == None:
-        sum_value = 0
-    remaining_words = project.total_count - sum_value
-    remaining_days = (project.end_date - datetime.date.today()).days
-    time_from_start = (datetime.date.today() - project.start_date).days
-    total_days = (project.end_date - project.start_date).days
-    try:
-        words_per_day = int(remaining_words / remaining_days)
-    except ZeroDivisionError:
-        words_per_day = int(remaining_words)
-    try:
-        date_progress = (time_from_start / total_days) * 100
-    except ZeroDivisionError:
-        # If the remaining days is 0, then we want the bar to be full
-        date_progress = 100
-    if request.method == "POST":
-        form = CountForm(request.POST)
+
+    def get(self, request, pk):
+        projects = Project.objects.all()
+        project = get_object_or_404(Project, pk=pk)
+        counts = project.counts.all()
+        count_sum = project.counts.aggregate(Sum('count_update'))
+        form = self.form_class
+        # Variables for date_progress and words_per_day
+        sum_value = next(iter(count_sum.values()))
+        if sum_value == None:
+            sum_value = 0
+        remaining_words = project.total_count - sum_value
+        remaining_days = (project.end_date - datetime.date.today()).days
+        time_from_start = (datetime.date.today() - project.start_date).days
+        total_days = (project.end_date - project.start_date).days
+        try:
+            words_per_day = int(remaining_words / remaining_days)
+        except ZeroDivisionError:
+            words_per_day = int(remaining_words)
+        try:
+            date_progress = (time_from_start / total_days) * 100
+        except ZeroDivisionError:
+            # If the remaining days is 0, then we want the bar to be full
+            date_progress = 100
+        return render(request, self.template_name, {
+            'projects': projects,
+            'project': project,
+            'counts': counts,
+            'count_sum': count_sum,
+            'date_progress': date_progress,
+            'words_per_day': words_per_day,
+            'form': form
+            })
+
+    def post(self, request, pk):
+        form = self.form_class(request.POST)
         if form.is_valid():
             count = form.save(commit=False)
             count.project = project
             count.save()
             return HttpResponseRedirect(reverse('project_detail', args=(project.id,)))
-    else:
-        form = CountForm()
-    return render(request, 'tracker/project_detail.html', {
-        'projects': projects,
-        'project': project,
-        'counts': counts,
-        'count_sum': count_sum,
-        'date_progress': date_progress,
-        'words_per_day': words_per_day,
-        'form': form
-        })
+        return render(request, self.template_name, {
+            'projects': projects,
+            'project': project,
+            'counts': counts,
+            'count_sum': count_sum,
+            'date_progress': date_progress,
+            'words_per_day': words_per_day,
+            'form': form
+            })
 
 @login_required
 def project_new(request):
